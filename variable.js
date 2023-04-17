ffc87889daa8d25ec1879562f751941e70888577
Songül ÇULUKEN <songulculuken97@gmail.com>
2023-04-17 10:22:31 +0300

    first commit

---
+++ variable.js
@@ -0,0 +1,14 @@
+let firstName ='Songül'
+let lastName ='Çuluken'
+let country = 'Türkiye'
+let city = 'İstanbul'
+let age = 25
+let isMarried = false
+
+const gravity = 9.81
+const boilingPoint = 100
+const PI = 3.14 
+
+let name= 'Songül',
+    job ='Junior Software Developer', 
+    live = 'Türkiye'
