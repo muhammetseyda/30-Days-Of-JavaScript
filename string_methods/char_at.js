c37f860abd0feca593e96441f42c09f1e01f6820
Songül ÇULUKEN <songulculuken97@gmail.com>
2023-04-17 11:38:12 +0300

    .

---
+++ string_methods/char_at.js
@@ -0,0 +1,5 @@
+string.charAt(index)
+let string = '30 Days Of JavaScript'
+console.log(string.charAt(0)) //3
+let lastIndex = string. length -1
+console.log(string.charAt(lastIndex)) // t
---
+++ string_methods/char_code_at.js
@@ -0,0 +1,5 @@
+string.charCodeAt(index)
+let string = '30 Days Of JavaScript'
+console.log(string.charCodeAt[30]) // D
+let lastIndex = string.length -1 
+console.log(string.charCodeAt(lastIndex)) // t
